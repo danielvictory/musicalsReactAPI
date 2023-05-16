b1c9b54d7afd44811a727cee7ae470e0093ea7c0
danielvictory <danieljvictory@gmail.com>
2023-05-16 10:26:13 -0500

    import Index to Main - still testing netlify deployment

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import {Route, Routes} from "react-router-dom";
 
+import Index from '../pages/Index'
+
 const Main = () => {
     const [musicals, setMusicals] = useState(null);
 
