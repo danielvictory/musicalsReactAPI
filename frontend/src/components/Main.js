a4802036d42701c14f7b745c1d26f0ef91528bbc
danielvictory <danieljvictory@gmail.com>
2023-05-16 10:08:40 -0500

    change data url to pull from deployed render.com backend site

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
+    const [musicals, setMusicals] = useState(null);
+
+    const URL = "https://musicalsreactapi.onrender.com";
+
+    const getMusicals = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setPeople(data);
+    }
+
+    const createMusicals = async (musical) => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(person),
+        });
+        getMusicals();
+    }
+
+    return (
+        <div>Main</div>
+    )
 }
 
 export default Main
