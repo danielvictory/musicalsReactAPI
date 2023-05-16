3b40d25fc8799a9524c5259316d66858c667f507
danielvictory <danieljvictory@gmail.com>
2023-05-16 10:44:49 -0500

    add data feed in to index page from backend (on render.com, data from mongodb)

--- frontend/src/pages/Index.js
+++ frontend/src/pages/Index.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+//import {Link} from 'react-router-dom'
 
-const Index = () => {
-  return (
-    <div>Index</div>
-  )
+const Index = (props) => {
+    
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+
+    const loaded = () => {
+        return props.musicals.map((musical) => (
+            <div key={musical._id} className="musical-container">
+                <Link to={`/musicals/${musical._id}`}>
+                    <h1>{musical.name}</h1>
+                </Link>
+                <img src={musical.image} alt={musical.name} />
+                <h3>{musical.premiereYear}</h3>
+            </div>
+        ));
+    }
+
+    return (
+        <section>{props.musicals ? loaded() : loading()}</section>
+        
+    )
 }
 
 export default Index
